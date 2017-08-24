7ad191edb22f9d71b934228e415e51be3a98291c
qtotuan <quynh.totuan@gmail.com>
2017-08-24 08:49:53 -0400

    Renamed app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ require 'rack/cors'
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://evening-sands-15145.herokuapp.com', 'http://localhost:3001'
+    origins 'https://track-your-habits.herokuapp.com', 'http://localhost:3001'
 
     resource '*',
       headers: :any,
