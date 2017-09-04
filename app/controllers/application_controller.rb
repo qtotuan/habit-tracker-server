527b026644c7a2ab814ec9d3b341002bee05ca03
qtotuan <quynh.totuan@gmail.com>
2017-09-04 13:46:46 -0400

    Add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,34 +11,39 @@ class ApplicationController < ActionController::API
     JWT.encode(payload, secret, algorithm)
   end
 
+  # This method will be called before the auth#show method to intercept that #show renders
+  # in case the user is not found in the database
   def authorize_user!
     if !current_user.present?
       render json: {error: 'No user id present'}
     end
   end
 
+  # The method is called by auth#show which renders the json object with the user info
   def current_user
     @current_user ||= User.find_by(id: token_user_id)
   end
 
+  # Helper method for current_user
   def token_user_id
     decoded_token.first['id']
   end
 
-    def decoded_token
-      if token
-        begin
-          # puts `----------------> JWT.decode #{JWT.decode(token, secret, true, {algorithm: algorithm})}`
-          JWT.decode(token, secret, true, {algorithm: algorithm})
-        rescue JWT::DecodeError
-          return [{}]
-        end
-      else
-        [{}]
+  # Helper method for token_user_id
+  def decoded_token
+    if token
+      begin
+        JWT.decode(token, secret, true, {algorithm: algorithm})
+      rescue JWT::DecodeError
+        return [{}]
       end
+    else
+      [{}]
     end
+  end
 
   # The jwt token from the POST request that the user is sending with each fetch
+  # Helper method for decoded_token
   def token
     request.headers['Authorization']
   end
